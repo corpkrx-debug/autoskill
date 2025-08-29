8a800290bb8bcdc9aee847422a050a793d58691f
jivuu0 <123092990+jivuu0@users.noreply.github.com>
2025-08-28 22:33:42 -0300

    Update JP_AutoSkill_v3.lua

--- JP_AutoSkill_v3.lua
+++ JP_AutoSkill_v3.lua
@@ -29,6 +29,7 @@ local CONFIG = {
 		["Saito e Gazo"] = {1, 4, 2, 3},
 		["Koju"] = {1, 3, 4, 2},
 		["Lufus"] = {1, 4, 3, 2	}
+		["TesteFoxuro"] = {4, 1, 2, 3},
     },
 
     FastTriggerAbilities = {
